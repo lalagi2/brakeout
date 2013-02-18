ac9a75c794fea4564d53b69aa5f5dd02525f615b
lalagi <tothlajosg@gmail.com>
2013-02-18 20:04:00 +0100

    das

---
+++ breakout/src/breakout.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : breakout.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+ssh-dss AAAAB3NzaC1kc3MAAACBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAAAAFQCXYFCPFSMLzLKSuYKi64QL8Fgc9QAAAIEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoAAACATKhY9yYZRDpygW7kufx0d/Ooe0Oqcb+RApo0aAV3wjIo0uRMqZ6Akp8OY0hIIy3LdfWIbpgS8JFzEKeAQHlIKtiZ8z1TRgAuFNZtkAmbqJSSSFfuWAeG6GAdG/utG+rH9V2Datx//Xn6u8/yhaFUgni/iYX8d5LSjuwNtp2UURw= DSA-1024
